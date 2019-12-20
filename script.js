60f01119fa4f51bc56f15ca52257e0921845c5e6
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-12-20 11:44:25 +0100

    Add JS to the code

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const panels = document.querySelectorAll('.panels');
+const panels = document.querySelectorAll('.panel');
 
-function toogleOpen(){
+function toggleOpen(){
   this.classList.toggle('open');
 }
+
+function toggleActive(e){
+  if (e.propertyName.includes('flex')){
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
